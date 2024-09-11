7e01ccd4017fd365981405286b258b5b607ef4f7
griseldagomez <grinoe123@gmail.com>
2024-09-11 11:33:06 +0200

    getfilter2

--- src/index.js
+++ src/index.js
@@ -44,43 +44,43 @@ server.get("/movies", async (req, res) => {
    
   // });
 
-  const fakeMovies = [
-    {
-      id: 1,
-      title: "Wonder Woman",
-      genre: "Action",
-      image:
-        "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/12/gal-gadot-como-wonder-woman-universo-extendido-dc-2895594.jpg?tf=3840x",
-      category: "Superhero",
-      year: 2017,
-      director: "Patty Jenkins",
-    },
-    {
-      id: 2,
-      title: "Inception",
-      genre: "Science Fiction",
-      image:
-        "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
-      category: "Thriller",
-      year: 2010,
-      director: "Christopher Nolan",
-    },
-    {
-      id: 3,
-      title: "Padre no hay más que uno 4",
-      genre: "Comedia",
-      image:
-        "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
-      category: "Thriller",
-      year: 2024,
-      director: "Christopher Nolan",
-    },
-  ];
+  // const fakeMovies = [
+  //   {
+  //     id: 1,
+  //     title: "Wonder Woman",
+  //     genre: "Action",
+  //     image:
+  //       "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/12/gal-gadot-como-wonder-woman-universo-extendido-dc-2895594.jpg?tf=3840x",
+  //     category: "Superhero",
+  //     year: 2017,
+  //     director: "Patty Jenkins",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Inception",
+  //     genre: "Science Fiction",
+  //     image:
+  //       "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
+  //     category: "Thriller",
+  //     year: 2010,
+  //     director: "Christopher Nolan",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Padre no hay más que uno 4",
+  //     genre: "Comedia",
+  //     image:
+  //       "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
+  //     category: "Thriller",
+  //     year: 2024,
+  //     director: "Christopher Nolan",
+  //   },
+  // ];
 
-  res.json({
-    success: true,
-    movies: fakeMovies,
-  });
+  // res.json({
+  //   success: true,
+  // //   movies: fakeMovies,
+  // });
 });
 
 // init express aplication
